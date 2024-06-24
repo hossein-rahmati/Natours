5e38ab27b9f099f45af4b1dfc312144db0120bdd
hossein rahmati <rmthossein@gmail.com>
2024-06-25 01:08:32 +0330

    update app.js - develope get & post on /tours endpoint

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const express = require('express');
+const fs = require('fs');
 
+const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.json({ message: 'Testing post method' });
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 201,
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
