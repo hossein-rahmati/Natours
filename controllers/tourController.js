9bee40fe027e5c16aff88098980b7b9b8133f65d
hossein rahmati <rmthossein@gmail.com>
2024-07-13 21:27:55 +0330

    control query requests

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFileds = ['page', 'sort', 'limit', 'fields'];
+    excludedFileds.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
