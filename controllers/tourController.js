730d544e9040f66bbef83c01c6083388350c2497
hossein rahmati <rmthossein@gmail.com>
2024-07-16 23:24:51 +0330

    pagination feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,11 +7,13 @@ exports.getAllTours = async (req, res) => {
     const excludedFileds = ['page', 'sort', 'limit', 'fields'];
     excludedFileds.forEach((el) => delete queryObj[el]);
 
+    // filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     let query = Tour.find(JSON.parse(queryStr));
 
+    // sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
@@ -19,6 +21,7 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // field limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
@@ -26,11 +29,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    // pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page is not exist');
+      }
+    }
 
     const tours = await query;
 
