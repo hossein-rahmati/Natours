cea2f382a45f13e429287f6f033206c79da15702
hossein rahmati <rmthossein@gmail.com>
2024-07-20 11:28:46 +0330

    adding aggregate middleware to hide secret tours of stats result

--- models/tourModel.js
+++ models/tourModel.js
@@ -91,6 +91,11 @@ tourSchema.pre(/^find/, function (next) {
 //   next();
 // });
 
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
