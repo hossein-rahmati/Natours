9b4951d7ffa823f9e20fed8ec5799c5003f9741a
hossein rahmati <rmthossein@gmail.com>
2024-07-22 23:37:35 +0330

    adding appError class & refactoring error hadnling code

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userTouter');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
-// MIDDLEWARES
 const app = express();
 
 app.use(express.json());
@@ -12,7 +13,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
@@ -22,21 +22,13 @@ app.all('*', (req, res, next) => {
   //   message: `Can't find ${req.originalUrl} on this server`,
   // });
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
