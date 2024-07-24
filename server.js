3482821b87dca6f6a225a4ad1007ddd34d047fd3
hossein rahmati <rmthossein@gmail.com>
2024-07-25 00:05:36 +0330

    handling 'unhandler rejection' & 'uncaught exception' errors

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -19,6 +25,15 @@ mongoose
   .then(() => console.log('DB connected successfully'));
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is listening on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    //Server.close => Does all pending requests and then closes the server
+    process.exit(1);
+  });
 });
