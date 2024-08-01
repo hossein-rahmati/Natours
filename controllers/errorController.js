20c90464745eb69095ab47521451a975ed4d9a4d
hossein-rahmati <rmthossein@gmail.com>
2024-08-01 20:38:33 +0330

    Protecting tour routes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,6 +18,12 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTInvalidError = () =>
+  new AppError('Invalid token. Please log in again', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Token expired. Please log in again', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -54,6 +60,8 @@ module.exports = (err, req, res, next) => {
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFiledsDB(err);
     if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
+    if (err.name === 'JsonWebTokenError') err = handleJWTInvalidError();
+    if (err.name === 'TokenExpiredError') err = handleJWTExpiredError();
 
     sendErrorProd(err, res);
   }
