32648e57b63964177772d24f59553f37c7697cf8
hossein-rahmati <rmthossein@gmail.com>
2024-08-06 13:23:16 +0330

    handling updateMe route (updating user data)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -8,6 +17,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     status: 'success',
     results: users.length,
     data: users,
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // create error if user posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for pasword updates. Please use /updatePassword',
+      ),
+      400,
+    );
+  }
+
+  // update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
 });
 
